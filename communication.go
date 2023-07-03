1f59c3e65966486f5508012aa60fb9e87909dc93
vudatny <vitek.udatny@protonmail.com>
2023-07-03 18:01:44 +0200

    pls

--- communication.go
+++ communication.go
@@ -476,3 +476,32 @@ func (m *Communication_Message_NACK) Encode() string {
 	encoded, _ := json.Marshal(m)
 	return string(encoded)
 }
+
+// custom protocol
+
+type Communication_Message_NavigationData_Report struct {
+	Latitude  float64
+	Longitude float64
+	Altitude  float64
+	VN        float64
+	VE        float64
+	VD        float64
+	IAS       float64
+	Heading   float64
+	Track     float64
+	Roll      float64
+	Pitch     float64
+	RollRate  float64
+	PitchRate float64
+}
+
+func (m *Communication_Message_NavigationData_Report) GetType() uint {
+	return 3
+}
+func (m *Communication_Message_NavigationData_Report) GetSubType() uint {
+	return 1
+}
+func (m *Communication_Message_NavigationData_Report) Encode() string {
+	encoded, _ := json.Marshal(m)
+	return string(encoded)
+}
