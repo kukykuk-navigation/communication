a588a7136187f8a17b4edbd8e6352f7c5b372fca
vudatny <vitek.udatny@protonmail.com>
2023-07-03 17:55:53 +0200

    added Communication_Message_NavigationData_Report

--- protocol.go
+++ protocol.go
@@ -1,43 +1,30 @@
 package communication
 
-type Communication_Message_ControlMode_Set struct {
-	ControlMode uint
+import "encoding/json"
+
+type Communication_Message_NavigationData_Report struct {
+	Latitude  float64
+	Longitude float64
+	Altitude  float64
+	VN        float64
+	VE        float64
+	VD        float64
+	IAS       float64
+	Heading   float64
+	Track     float64
+	Roll      float64
+	Pitch     float64
+	RollRate  float64
+	PitchRate float64
 }
 
-type Communication_Message_ControlMode_Report struct {
-	ControlMode uint
+func (m *Communication_Message_NavigationData_Report) GetType() uint {
+	return 3
 }
-
-type Communication_Message_TargetTracking_Set struct {
-	CenterX float64
-	CenterY float64
+func (m *Communication_Message_NavigationData_Report) GetSubType() uint {
+	return 1
 }
-
-type Communication_Message_TargetTracking_Get struct {
-	XMin float64
-	XMax float64
-	YMin float64
-	YMax float64
-}
-
-type Communication_Message_TargetTrackingStatus struct {
-	Status bool
-}
-
-type Communication_Message_GuidanceState struct {
-	DistanceToNext float64
-	HeadingToNext  float64
-}
-
-type Communication_Message_BatteryVoltage struct {
-	Voltage float64
-}
-
-type Communication_Message_OnboardSystems struct {
-	Video1In       uint
-	Video2In       uint
-	TargetTracking uint
-	FCRX           uint
-	FCTX           uint
-	ControlLoop    uint
+func (m *Communication_Message_NavigationData_Report) Encode() string {
+	encoded, _ := json.Marshal(m)
+	return string(encoded)
 }
