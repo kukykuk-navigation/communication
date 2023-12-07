24d57692b77e4535af1d63c0344e11523c28d1aa
vudatny <vitek.udatny@protonmail.com>
2023-12-07 22:46:25 +0100

    paths separated

--- protocol.go
+++ protocol.go
@@ -81,22 +81,19 @@ type Communication_NavigationPointGlobal struct {
 }
 
 type Communication_Message_NavigationData_Report struct {
-	Latitude   float64
-	Longitude  float64
-	Altitude   float64
-	VN         float64
-	VE         float64
-	VD         float64
-	IAS        float64
-	Heading    float64
-	Track      float64
-	Roll       float64
-	Pitch      float64
-	RollRate   float64
-	PitchRate  float64
-	GNSSPath   []Communication_NavigationPointGlobal
-	GlobalPath []Communication_NavigationPointGlobal
-	LocalPath  []Communication_NavigationPointLocal
+	Latitude  float64
+	Longitude float64
+	Altitude  float64
+	VN        float64
+	VE        float64
+	VD        float64
+	IAS       float64
+	Heading   float64
+	Track     float64
+	Roll      float64
+	Pitch     float64
+	RollRate  float64
+	PitchRate float64
 }
 
 func (m *Communication_Message_NavigationData_Report) GetType() uint {
@@ -110,19 +107,48 @@ func (m *Communication_Message_NavigationData_Report) Encode() string {
 	return string(encoded)
 }
 
-type Communication_Message_NavigationPath_Report struct {
+type Communication_Message_NavigationGNSSPath_Report struct {
 	GNSSPath   []Communication_NavigationPointGlobal
 	GlobalPath []Communication_NavigationPointGlobal
-	LocalPath  []Communication_NavigationPointLocal
 }
 
-func (m *Communication_Message_NavigationPath_Report) GetType() uint {
+func (m *Communication_Message_NavigationGNSSPath_Report) GetType() uint {
 	return 3
 }
-func (m *Communication_Message_NavigationPath_Report) GetSubType() uint {
+func (m *Communication_Message_NavigationGNSSPath_Report) GetSubType() uint {
 	return 2
 }
-func (m *Communication_Message_NavigationPath_Report) Encode() string {
+func (m *Communication_Message_NavigationGNSSPath_Report) Encode() string {
+	encoded, _ := json.Marshal(m)
+	return string(encoded)
+}
+
+type Communication_Message_NavigationGlobalPathEstimation_Report struct {
+	GlobalPathEstimation []Communication_NavigationPointGlobal
+}
+
+func (m *Communication_Message_NavigationGlobalPathEstimation_Report) GetType() uint {
+	return 3
+}
+func (m *Communication_Message_NavigationGlobalPathEstimation_Report) GetSubType() uint {
+	return 3
+}
+func (m *Communication_Message_NavigationGlobalPathEstimation_Report) Encode() string {
+	encoded, _ := json.Marshal(m)
+	return string(encoded)
+}
+
+type Communication_Message_NavigationLocalPathEstimation_Report struct {
+	LocalPathEstimation []Communication_NavigationPointLocal
+}
+
+func (m *Communication_Message_NavigationLocalPathEstimation_Report) GetType() uint {
+	return 3
+}
+func (m *Communication_Message_NavigationLocalPathEstimation_Report) GetSubType() uint {
+	return 4
+}
+func (m *Communication_Message_NavigationLocalPathEstimation_Report) Encode() string {
 	encoded, _ := json.Marshal(m)
 	return string(encoded)
 }
