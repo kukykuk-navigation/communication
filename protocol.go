bea60e5f31687b9df97384d26cd2259b66c120a8
vudatny <vitek.udatny@protonmail.com>
2023-12-08 00:06:02 +0100

    gnss navigation report

--- protocol.go
+++ protocol.go
@@ -81,19 +81,20 @@ type Communication_NavigationPointGlobal struct {
 }
 
 type Communication_Message_NavigationData_Report struct {
-	Latitude  float64
-	Longitude float64
-	Altitude  float64
-	VN        float64
-	VE        float64
-	VD        float64
-	IAS       float64
-	Heading   float64
-	Track     float64
-	Roll      float64
-	Pitch     float64
-	RollRate  float64
-	PitchRate float64
+	Latitude       float64
+	Longitude      float64
+	Altitude       float64
+	VN             float64
+	VE             float64
+	VD             float64
+	IAS            float64
+	Heading        float64
+	Track          float64
+	Roll           float64
+	Pitch          float64
+	RollRate       float64
+	PitchRate      float64
+	GNSSNavigation float64
 }
 
 func (m *Communication_Message_NavigationData_Report) GetType() uint {
